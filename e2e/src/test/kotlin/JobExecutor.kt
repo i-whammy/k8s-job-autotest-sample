1fea2a41cc6f8ac59c09e0ce2fb709630145abe8
i-whammy <yas.iwami@gmail.com>
2025-02-09 21:55:32 +0900

    read properties file

--- e2e/src/test/kotlin/JobExecutor.kt
+++ e2e/src/test/kotlin/JobExecutor.kt
@@ -1,5 +1,6 @@
 import com.thoughtworks.gauge.Step
 import java.io.File
+import java.util.ResourceBundle
 
 interface JobExecutor {
     fun execute()
@@ -7,7 +8,7 @@ interface JobExecutor {
 
 class SkaffoldRunner: JobExecutor {
     private val skaffoldYamlDirectory = File("../environments")
-    private val imageTag = "stable" // TODO: Read config file and change tag
+    private val imageTag = ResourceBundle.getBundle("e2e").getString("imageTag")
 
     @Step("Run k8s job with skaffold")
     override fun execute() {
@@ -26,5 +27,6 @@ class SkaffoldRunner: JobExecutor {
         val pipelines = ProcessBuilder.startPipeline(processBuilders)
         val result = pipelines.last().waitFor()
         println("${runner.command().joinToString(" ")} finishes with status code $result")
+        if (result > 0) throw RuntimeException("${runner.command().joinToString(" ")} with error.")
     }
 }
